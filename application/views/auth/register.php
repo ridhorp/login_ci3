81b7e27768bbe8f176a46d03b3cf1de801f0ee2a
Ridho RP <ridhogoodevil@gmail.com>
2022-11-25 07:15:06 +0700

    first commit

---
+++ application/views/auth/register.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Login &mdash; Stisla</title>
+
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="<?php echo base_url() . 'assets/' ?>modules/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() . 'assets/' ?>modules/fontawesome/css/all.min.css">
+
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="<?php echo base_url() . 'assets/' ?>modules/bootstrap-social/bootstrap-social.css">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="<?php echo base_url() . 'assets/' ?>css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url() . 'assets/' ?>css/components.css">
+</head>
+
+<body>
+
+
+    <!-- General JS Scripts -->
+    <script src="<?php echo base_url() . 'assets/' ?>modules/jquery.min.js"></script>
+    <script src="<?php echo base_url() . 'assets/' ?>modules/popper.js"></script>
+    <script src="<?php echo base_url() . 'assets/' ?>modules/tooltip.js"></script>
+    <script src="<?php echo base_url() . 'assets/' ?>modules/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url() . 'assets/' ?>modules/nicescroll/jquery.nicescroll.min.js"></script>
+    <script src="<?php echo base_url() . 'assets/' ?>modules/moment.min.js"></script>
+    <script src="<?php echo base_url() . 'assets/' ?>js/stisla.js"></script>
+
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+
+    <!-- Template JS File -->
+    <script src="<?php echo base_url() . 'assets/' ?>js/scripts.js"></script>
+    <script src="<?php echo base_url() . 'assets/' ?>js/custom.js"></script>
+</body>
+
+</html>
